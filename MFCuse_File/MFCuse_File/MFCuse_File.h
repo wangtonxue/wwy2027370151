91980075847fade224b27db8a0a76d9b062c2ce0
wangtonxue <129303263+wangtonxue@users.noreply.github.com>
2023-06-25 21:24:30 +0800

    Add files via upload

---
+++ MFCuse_File/MFCuse_File/MFCuse_File.h
@@ -0,0 +1,32 @@
+﻿
+// MFCuse_File.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCuseFileApp:
+// 有关此类的实现，请参阅 MFCuse_File.cpp
+//
+
+class CMFCuseFileApp : public CWinApp
+{
+public:
+	CMFCuseFileApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCuseFileApp theApp;
